0a23d81ea20c5e1e11b9cd4d5decb4bf5cfacee5
Paul-Elliot <peada@free.fr>
2026-08-28 19:24:49 +0200

    Fix getDownloadURL

--- dist/index.js
+++ dist/index.js
@@ -33065,7 +33065,7 @@ async function setup() {
   debug(downloadURL);
 
   // Download the specific version of the tool, e.g. as a tarball
-  const pathToTarball = await downloadTool(getDownloadURL());
+  const pathToTarball = await downloadTool(downloadURL);
 
   // Extract the tarball onto the runner
   const pathToCLI = await extractTar(pathToTarball, undefined, "x");
--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ async function setup() {
   core.debug(downloadURL);
 
   // Download the specific version of the tool, e.g. as a tarball
-  const pathToTarball = await tc.downloadTool(getDownloadURL());
+  const pathToTarball = await tc.downloadTool(downloadURL);
 
   // Extract the tarball onto the runner
   const pathToCLI = await tc.extractTar(pathToTarball, undefined, "x");
